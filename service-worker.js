7831923921dc5f604968f9624a98fd9b1d2a8091
Julio Cezar <chupym.julio@gmail.com>
2026-03-02 14:21:13 -0300

    Fix: Incrementando versão do Service Worker para forçar limpeza de cache do app name

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "editor-taurus-v1";
+const CACHE_NAME = "editor-taurus-v2";
 const ASSETS = [
   "./index.html",
   "./css/main.css",
@@ -18,6 +18,20 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
     }),
   );
 });
